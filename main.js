dc2db2be46ac6fdd3909694d701dbc033639b768
cabal82 <fabriziogallici@gmail.com>
2019-11-18 14:56:59 +0100

    es. base ok

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ Se è <= 5 il quadrato diventa giallo,
   se è > di 5 il quadrato diventa verde.
 Il numero ottenuto appare al centro del quadrato */
 
-
 $(document).ready(function () {
+
+  $('.circle').on('click', function () {
+    var $curCircle = $(this); //salvo relativo valore this current circle per poterlo usare all'interno ajax
+
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int ",
+      method: "GET",
+      success: function (data) {
+        // console.log(data);
+        var rndNum = data.response;
+        $curCircle.text(rndNum);
+        if (rndNum <= 5) {
+          $curCircle.css("background-color", "yellow");
+        } else if (rndNum > 5) {
+          $curCircle.css("background-color", "green");
+        }
+      },
+      error: function (richiesta, stato) {
+        console.log("c'è stato un errore: " + stato);
+
+      }
+
+    });
+
+  })
 
 });
